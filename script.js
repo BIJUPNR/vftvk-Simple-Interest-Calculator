c221dcaff59394a5a5a35cc007ee1fc437c11a21
Biju Palliyath <bijupnr@outlook.com>
2022-01-25 08:09:25 +0530

    updated after testing

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 function compute()
 {
-      
+    
+    var result = document.getElementById("result"); 
+    result.innerHTML = "";
+
+    if (document.getElementById("principal").value.length == 0){
+        alert("Enter a positive number");        
+        return false;
+    }
+    
     var principal =  parseFloat(document.getElementById("principal").value);  
     
     if (principal < 1){        
         alert ("Enter a positive number"); 
-        principal.focus();       
+        document.getElementById("principal").focus;     
         return false;
     }
+    
     
     var rate = parseFloat(document.getElementById("rate").value);     
     var years = parseInt(document.getElementById("years").value);    
     var interest = principal * years * rate / 100;
-    var year = new Date().getFullYear() + parseInt(years);   
-    var total = principal + interest;
-    var result = document.getElementById("result");   
+    var year = new Date().getFullYear() + parseInt(years); 
 
     result.innerHTML = `If you deposit <mark>${principal}</mark>,<br/>at an interest rate of <mark>${rate}%</mark>.<br/> 
-    You will receive an amount of <mark>${total}</mark>,<br/> in the year ${year}`;
+    You will receive an amount of <mark>${interest}</mark>,<br/> in the year ${year}`;
         
 }
 
